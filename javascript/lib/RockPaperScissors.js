1e0b47816f0b0636af7ea19e0a4ef49ee4948ff1
benjamintillett <benjamin.tillett@gmail.com>
2014-09-15 12:00:30 +0100

    git ci -m passing all tests

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -1,17 +1,34 @@
 function Player(name) {
   this.name = name;
-}
+  this.pick = null;
+};
+
 
 function Game(player1, player2) {
   this.player1 = player1;
   this.player2 = player2;
+  this.PAIRS = {
+    rock:     { beats: 'scissors' },
+    paper:    { beats: 'rock' },
+    scissors: { beats: 'paper' }
+  };
 }
 
 
+
 Player.prototype.picks = function(pick) {
-  // body...
+  	this.pick = pick
 };
 
 Game.prototype.winner = function() {
-    return this.player1;
+	if (this.player1.pick === this.player2.pick) {
+		return null
+	}
+	else if (this.PAIRS[this.player1.pick]['beats'] === this.player2.pick){
+    	return this.player1;
+	}
+	else {
+		return this.player2;
+	};
+
 };
